5a64fbde63690fad3ed8ec13102b8faaa48f636c
vincenzomartello <vmartello.95@gmail.com>
2020-06-05 19:05:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
-from distutils.core import setup
-setup(
-  name = 'erexplain',         
-  packages = ['erxplain'],  
-  version = '0.1.1',     
-  license='apache-2.0',  
-  description = 'library to explain models for Entity Resolution', 
-  author = 'Vincenzo Martello',
-  author_email = 'v.martello@libero.it',
-  url = 'https://github.com/vincenzomartello/ERExplain.git',
-  download_url = 'https://github.com/vincenzomartello/ERExplain/archive/0.1.1.tar.gz', 
-  keywords = ['ML', 'Entity-Resolution', 'explainable-ai'],
-  install_requires=[
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="erexplain", # Replace with your own username
+    version="0.1.2",
+    author="Vincenzo Martello",
+    author_email="v.martello@libero.it",
+    description="library to explain models for Entity Resolution",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url= 'https://github.com/vincenzomartello/ERExplain.git',
+    download_url = 'https://github.com/vincenzomartello/ERExplain/archive/0.1.2.tar.gz'
+    packages=['erexplain'],
+    install_requires=[
           'pandas',
           'numpy',
           'tqdm',
           'mlxtend>=0.17.2'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers', 
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-  ],
+      ]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: Apache Software License',
+        "Operating System :: OS Independent",
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    python_requires='>=3.6',
 )
